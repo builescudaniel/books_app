ca14d4bd5ed864846fd953924bd649eeb1463b01
daniel <danielbuilescu@gmail.com>
2023-09-26 18:23:03 +0300

    update

--- book_app/views.py
+++ book_app/views.py
@@ -2,8 +2,21 @@ from django.shortcuts import render
 from .models import Book
 from django_ratelimit.decorators import ratelimit
 
+from django.http import HttpRequest
+
+def get_client_ip(request, view):
+    if not isinstance(request, HttpRequest):
+        return "unknown"
+
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0].strip()
+    else:
+        ip = request.META.get('REMOTE_ADDR', "unknown")
+    return ip
+
 # Create your views here.
-@ratelimit(key='ip', rate='5/m', block=True)
+@ratelimit(key=get_client_ip, rate='5/m', method='GET', block=True)
 def index(request):
     books = Book.objects.all()
     context = {'books': books}
