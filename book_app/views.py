3ac278f16090d3ad25a300776618595f28eaae07
daniel <danielbuilescu@gmail.com>
2023-09-26 17:18:55 +0300

    update

--- book_app/views.py
+++ book_app/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from .models import Book
+from django_ratelimit.decorators import ratelimit
 
 # Create your views here.
+@ratelimit(key='ip', rate='5/m', block=True)
 def index(request):
     books = Book.objects.all()
     context = {'books': books}
--- book_project/settings.py
+++ book_project/settings.py
@@ -57,6 +57,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django_ratelimit.middleware.RatelimitMiddleware',
 ]
 
 ROOT_URLCONF = 'book_project.urls'
